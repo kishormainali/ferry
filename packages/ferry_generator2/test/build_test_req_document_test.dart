87cac0b95cadac6924ea38da60b711ba5fb55a4c
Martin Kamleithner <martin.kamleithner@gmail.com>
2026-01-01 23:21:20 +0000

    test(generator2): assert minimal req documents

---
+++ packages/ferry_generator2/test/build_test_req_document_test.dart
@@ -0,0 +1,106 @@
+@TestOn('vm')
+
+import 'package:build/build.dart';
+import 'package:build_test/build_test.dart';
+import 'package:ferry_generator2/graphql_builder.dart';
+import 'package:test/test.dart';
+
+const _package = 'ferry_generator2';
+const _schemaPath = '$_package|lib/schema.graphql';
+const _queryPath = '$_package|lib/queries.graphql';
+const _reqPath =
+    '.dart_tool/build/generated/$_package/lib/__generated__/queries.req.gql.dart';
+
+const _schema = r'''
+schema {
+  query: Query
+}
+
+type Author {
+  name: String!
+}
+
+type Book {
+  title: String!
+  author: Author!
+}
+
+type Query {
+  books: [Book!]!
+}
+''';
+
+const _document = r'''
+fragment TitleFields on Book {
+  title
+}
+
+fragment AuthorFields on Author {
+  name
+}
+
+query BooksA {
+  books {
+    ...TitleFields
+  }
+}
+
+query BooksB {
+  books {
+    author {
+      ...AuthorFields
+    }
+  }
+}
+''';
+
+void main() {
+  test('request documents include only needed definitions', () async {
+    final assets = <String, String>{
+      _schemaPath: _schema,
+      _queryPath: _document,
+    };
+
+    final builder = graphqlBuilder(
+      BuilderOptions({
+        'schema': {
+          'file': _schemaPath,
+        },
+      }),
+    );
+
+    final result = await testBuilder(
+      builder,
+      assets,
+      rootPackage: _package,
+      generateFor: {_queryPath},
+    );
+
+    expect(result.succeeded, isTrue);
+    final reqContents = await result.readerWriter.readAsString(
+      AssetId(_package, _reqPath),
+    );
+
+    final booksABlock = _classBlock(reqContents, 'GBooksAReq');
+    expect(booksABlock, contains('BooksA'));
+    expect(booksABlock, contains('TitleFields'));
+    expect(booksABlock, isNot(contains('BooksB')));
+    expect(booksABlock, isNot(contains('AuthorFields')));
+
+    final booksBBlock = _classBlock(reqContents, 'GBooksBReq');
+    expect(booksBBlock, contains('BooksB'));
+    expect(booksBBlock, contains('AuthorFields'));
+    expect(booksBBlock, isNot(contains('BooksA')));
+    expect(booksBBlock, isNot(contains('TitleFields')));
+  });
+}
+
+String _classBlock(String contents, String className) {
+  final start = contents.indexOf('class $className');
+  if (start == -1) {
+    throw StateError('Missing class $className in request output');
+  }
+  final next = contents.indexOf('class G', start + 1);
+  final end = next == -1 ? contents.length : next;
+  return contents.substring(start, end);
+}
